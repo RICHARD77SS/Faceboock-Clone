f584106aa809206cf09f12d9efdda67796354872
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-02 15:10:54 -0300

    Painel de exibição de Lives

---
+++ src/Components/WatchLive/index.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Input from '../Input';
+import Button from '../Button';
+
+import { MdVisibility } from 'react-icons/md';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { FaRegCommentAlt } from 'react-icons/fa';
+import { RiShareForwardLine } from 'react-icons/ri';
+import { BiLike, BiBookmarkAlt, BiRadioCircleMarked } from 'react-icons/bi';
+import { BsEmojiSmile, BsCamera } from 'react-icons/bs';
+import { MdOutlineGif } from 'react-icons/md';
+
+import { WatchLiveSt, LiveViewSt, LiveTopSt, LiveContentSt, LiveBottomSt, BlockSt, LiveViewBottomSt, PostCommentSt, InputsSt, InsertsSt } from './styles';
+
+export const WatchLive = () => {
+  const [comment, setComment] = React.useState(false);
+  return(
+    <WatchLiveSt>
+      <LiveViewSt>
+        <LiveTopSt>
+          <Button>
+            <div className="live">AO VIVO</div>
+            <div className="views"><MdVisibility /> 2399</div>
+          </Button>
+          <Button className='btn-options'>
+            <FiMoreHorizontal />
+          </Button>
+        </LiveTopSt>
+        <LiveContentSt>
+          <video src=""></video>
+        </LiveContentSt>
+        <LiveBottomSt>
+          <img src="https://img.freepik.com/vetores-gratis/cao-bonito-enfiando-a-lingua-para-fora-ilustracao-do-icone-dos-desenhos-animados_138676-2709.jpg?w=740&t=st=1662140788~exp=1662141388~hmac=de24c5a7eee813bc20233b227a5b0c3adf44fce5c0613e10b25c56cf3ead9112" alt="" />
+          <BlockSt>
+            <a href="/">User Name</a>
+            <a href="/">User Page</a>
+          </BlockSt>
+        </LiveBottomSt>
+        <LiveViewBottomSt>
+          <Button className='btn'>
+            <BiLike />
+            <p>Curtir</p>
+          </Button>
+          <Button className='btn' onClick={() => { setComment(true) }}>
+            <FaRegCommentAlt />
+            <p>Comentar</p>
+          </Button>
+          <Button className='btn'>
+            <RiShareForwardLine />
+            <p>Compartilhar</p>
+          </Button>
+        </LiveViewBottomSt>
+        {comment ?
+          <>
+            <PostCommentSt>
+              <InputsSt>
+                <img src="https://i.postimg.cc/brczWrGr/richardsilva.jpg" alt="usuario" />
+                <Input className='input' placeholder='  Escreva um comentario...' type='text' />
+                <InsertsSt>
+                  <Button className='btn-ico'>
+                    <BiRadioCircleMarked className='ico' />
+                  </Button>
+                  <Button className='btn-ico'>
+                    <BsEmojiSmile className='ico' />
+                  </Button>
+                  <Button className='btn-ico'>
+                    <BsCamera className='ico' />
+                  </Button>
+                  <Button className='btn-ico'>
+                    <MdOutlineGif className='ico' />
+                  </Button>
+                  <Button className='btn-ico'>
+                    <BiBookmarkAlt className='ico' />
+                  </Button>
+                </InsertsSt>
+              </InputsSt>
+            </PostCommentSt>
+          </> : null}
+        
+      </LiveViewSt>
+    </WatchLiveSt>
+  )
+}
+
+export default WatchLive;
---
+++ src/Components/WatchLive/styles.ts
@@ -0,0 +1,168 @@
+import Styled from 'styled-components';
+
+export const WatchLiveSt = Styled.div`
+  width: 95%;
+  height: 100%;
+`
+
+export const LiveViewSt = Styled.div`
+  width: 100%;
+  height: 500px;
+  border-radius: 5px;
+  background: ${Props => Props.theme.colors.background};
+`
+export const LiveTopSt = Styled.div`
+  width: 100%;
+  height: 50px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: .5rem;
+  transform: translatey(3rem);
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .live {
+    background: #f00;
+    animation-name: live;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    padding: .3rem;
+    color: #fff;
+    border-radius: 5px;
+  }
+  @keyframes live{
+    from {background-color: #f00;}
+    to {background-color: #800;}
+  }
+  .views {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    margin-left: .5rem;
+  }
+  .btn-options {
+    color: #fff;
+    font-size: 1.5rem;
+  }
+`
+export const LiveContentSt = Styled.div`
+  width: 100%;
+  height: 530px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #000;
+  transform: translatey(-3rem);
+`
+export const LiveBottomSt = Styled.div`
+  width: 20%;
+  height: 90px;
+  padding-left: 1.2rem;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  transform: translatey(-9.5rem);
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50px;
+  }
+`
+
+export const BlockSt = Styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  flex-direction: column;
+  a {
+    color: #f4f4f4;
+    text-decoration: none;
+    font-size: 1.2rem;
+    margin-left: 1rem;
+    :hover {
+      text-decoration: underline;
+    }
+  }
+  a:last-child{
+    font-size: .9rem;
+  }
+`
+export const LiveViewBottomSt = Styled.div`
+  width: 100%;
+  height: 50px;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  transform: translatey(-3rem);
+  background: ${Props => Props.theme.colors.background};
+  .btn {
+    display: flex;
+    align-items: center;
+    color: ${Props => Props.theme.colors.text};
+    font-size: 1.2rem;
+    margin-right: 1rem;
+    p {
+      margin-left: .4rem;
+      font-size: .9rem;
+    }
+
+  }
+`
+export const PostCommentSt = Styled.div`
+  width: 100%;
+  padding-bottom: 1rem;
+  background: ${Props => Props.theme.colors.background};
+  transform: translatey(-3rem);
+  .input{
+    width: 88%;
+    height: 30px;
+    border-radius: 20px;
+    border: none;
+    background: ${Props => Props.theme.colors.secundary};
+    :focus{
+      border: none;
+      outline: none;
+    }
+  }
+`
+export const InputsSt = Styled.div`
+  width: auto;
+  display: flex;
+  align-items: center;
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 40px;
+    margin: 0 .5rem;
+  }
+`
+export const InsertsSt = Styled.div`
+  width: auto;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  right: 5rem;
+  .btn-ico{
+    width: 25px;
+    height: 25px;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    :hover {
+      background: ${Props => Props.theme.colors.hover};
+    }
+  }
+  .ico {
+    font-size: 1rem;
+  }
+`
