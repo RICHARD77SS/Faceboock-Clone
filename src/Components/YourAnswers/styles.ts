5d6a88476e3f3d42bc9d07dfe5ee1ca5dbb99288
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-14 14:50:50 -0300

    Estilização Componente YourAnswers

--- src/Components/YourAnswers/styles.ts
+++ src/Components/YourAnswers/styles.ts
@@ -1,7 +1,15 @@
 import Styled from 'styled-components';
 
 export const YourAnswersSt = Styled.div`
+  width: 100%;
+  height: 300px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  padding: .5rem;
+  p {
+    font-size: .8rem;
+    text-align: center;
+  }
 `
