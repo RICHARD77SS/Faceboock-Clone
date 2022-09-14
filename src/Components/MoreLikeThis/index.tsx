fe8ef647b0c9d4d2f693f0bd9a780c2dcb9ea69e
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-09-14 14:46:31 -0300

    Extilização componente MoreLikeThis

--- src/Components/MoreLikeThis/index.tsx
+++ src/Components/MoreLikeThis/index.tsx
@@ -10,7 +10,35 @@ const MoreLikeThis = () => {
       <MoreLikeThisSt id='btn-4' className='content'>
         <VideoItem>
           <Image>
-            <img src="" alt="" />
+            <img src="https://img.freepik.com/fotos-gratis/pesos-de-exercicios-dumbbell-de-ferro-com-placas-extra_1423-223.jpg?w=740&t=st=1663176799~exp=1663177399~hmac=e448938cb04cde31ceb1a90d4fd2b9e81780e1e70ba68917016f87882d101f0d" alt="" />
+            <Duration>
+              1:22:22
+            </Duration>
+          </Image>
+          <Infos>
+            <h4>Video Name</h4>
+            <span>User Name</span>
+            <p>Há 20 horas 7,3 mi</p>
+          </Infos>
+          <Button><FiMoreHorizontal /></Button>
+        </VideoItem>
+        <VideoItem>
+          <Image>
+            <img src="https://img.freepik.com/fotos-gratis/pesos-de-exercicios-dumbbell-de-ferro-com-placas-extra_1423-223.jpg?w=740&t=st=1663176799~exp=1663177399~hmac=e448938cb04cde31ceb1a90d4fd2b9e81780e1e70ba68917016f87882d101f0d" alt="" />
+            <Duration>
+              1:22:22
+            </Duration>
+          </Image>
+          <Infos>
+            <h4>Video Name</h4>
+            <span>User Name</span>
+            <p>Há 20 horas 7,3 mi</p>
+          </Infos>
+          <Button><FiMoreHorizontal /></Button>
+        </VideoItem>
+        <VideoItem>
+          <Image>
+            <img src="https://img.freepik.com/fotos-gratis/pesos-de-exercicios-dumbbell-de-ferro-com-placas-extra_1423-223.jpg?w=740&t=st=1663176799~exp=1663177399~hmac=e448938cb04cde31ceb1a90d4fd2b9e81780e1e70ba68917016f87882d101f0d" alt="" />
             <Duration>
               1:22:22
             </Duration>
--- src/Components/MoreLikeThis/styles.ts
+++ src/Components/MoreLikeThis/styles.ts
@@ -3,13 +3,7 @@ import Styled from 'styled-components';
 export const Container = Styled.div`
   width: auto;
   height: 100vh;
-  
-  .content {
-    display: none;
-  }
-  .content.active {
-    display: flex;
-  }
+
   div::-webkit-scrollbar {
     width: 12px;
   }
@@ -31,10 +25,52 @@ export const MoreLikeThisSt = Styled.div`
   align-items: center;
 `
 export const VideoItem = Styled.div`
+  display: flex;
+  align-items: start;
+  margin: .5rem 0;
+  button{
+    width: 30px;
+    height: 30px;
+    border-radius: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    :hover {
+      background: #ddd;
+    }
+  }
 `
 export const Image = Styled.div`
+  width: 150px;
+  height: 80px;
+  img{
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+  }
 `
 export const Duration = Styled.div`
+  color: #fff;
+  position: absolute;
+  transform: translate(80px, -25px);
 `
 export const Infos = Styled.div`
+  height: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  margin: 0 .5rem;
+  h4 {
+    font-size: 1rem;
+    margin: .2rem 0;
+  }
+  span {
+    font-size: .9rem;
+    margin: .2rem 0;
+  }
+  p{
+    font-size: .8rem;
+    margin: .2rem 0;
+  }
 `
